8a4ec56a673052f921d1a4bcf0861abef694abe5
Claire Tregunna <c.tregunna1980@gmail.com>
2023-06-18 17:04:33 +0100

    JS functionality to add endorsements

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://ctendoresments-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsInDB = ref(database, "endorsements")
+
+
+const inputFieldEl = document.getElementById("input-field")
+const fromEl = document.getElementById("from-el")
+const toEl = document.getElementById("to-el")
+const publishBtn = document.getElementById("publish-btn")
+const endorsementContainer = document.getElementById("endorsement-container")
+
+publishBtn.addEventListener('click', function() {
+    let endorsementValue = inputFieldEl.value
+    let fromValue = fromEl.value
+    let toValue = toEl.value
+    let fullEndorsementObject = {
+        endorsement: endorsementValue,
+        from: fromValue,
+        to: toValue
+    }
+    push(endorsementsInDB, fullEndorsementObject)
+    clearInputFieldEl()
+})
+
+
+function clearEndorsements() {
+    endorsementContainer.innerHTML = "";
+}
+
+onValue(endorsementsInDB, function(snapshot){
+    if (snapshot.exists()) {
+        let endorsementsArray = Object.entries(snapshot.val()) //turns the content into an array
+        clearEndorsements()
+        for (let i = 0; i < endorsementsArray.length; i ++) {
+            appendEndorsement(endorsementsArray[i])
+            // console.log(endorsementsArray[i])
+        }
+    }
+    else {
+        endorsementContainer.innerHTML = "There are no endorsements yet, feel free to add one"
+    }
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value= ""
+    toEl.value = ""
+    fromEl.value = ""
+}
+
+// function to create the div containing the endorsment
+function appendEndorsement(theEndorsement) {
+    const endorsementID = theEndorsement[0]
+    const myEndorsement = theEndorsement[1]
+    // console.log(myEndorsement)
+    let newEndorsement = document.createElement("div")
+    newEndorsement.className = "end-div"
+    let endorsementText = myEndorsement.endorsement
+    let fromValue = myEndorsement.from
+    let toValue = myEndorsement.to
+    newEndorsement.innerHTML = `
+        <h4>To: ${toValue}</h4>
+        <p>${endorsementText}</p>
+        <h4>From: ${fromValue}</h4>
+        `
+    newEndorsement.addEventListener('click', function() {
+        let exactLocationOfEndorsementInDB = ref(database, `endorsements/${endorsementID}`)
+        remove(exactLocationOfEndorsementInDB)
+    })
+    endorsementContainer.appendChild(newEndorsement)
+}
+
